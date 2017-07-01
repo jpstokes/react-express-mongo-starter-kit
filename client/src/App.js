84b941153b842ca5e2c49464a90bfeeb70b10708
Jason Stokes <jpstokes@plecco.net>
2017-07-01 19:36:12 -0400

    Add subcomponents.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+const UserForm = (props) => (
+  <form id="userForm" method="POST">
+    <Row>
+      <Col xs={3}><input type="text" className="form-control" placeholder="First name" onChange={props.stateHandler} name="firstName"/></Col>
+      <Col xs={3}><input type="text" className="form-control" placeholder="Last name" onChange={props.stateHandler} name="lastName"/></Col>
+      <Col xs={2}><input type="text" className="form-control" placeholder="Username" onChange={props.stateHandler}  name="username"/></Col>
+      <Col xs={2}><input type="email" className="form-control" placeholder="Email" onChange={props.stateHandler} name="email"/></Col>
+      <Col xs={2}>
+        <button type="button" className="form-control" onClick={props.saveUser}>Submit</button>
+      </Col>
+    </Row>
+  </form>
+)
+
+const UserList = (props) => {
+  if (props.users.length !== 0) {
+    const users = props.users.map(user =>
+      <ul key={user._id}>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.username}</li>
+        <li>{user.email}</li>
+        <li onClick={props.deleteUser(user)}>Delete</li>
+      </ul>
+    )
+    return (
+      <div>
+        <h2>User List</h2>
+        {users}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
 
 class App extends Component {
   state = {users: []}
@@ -50,22 +86,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        <form id="userForm" method="POST">
-          <input type="text" placeholder="First name" onChange={this.stateHandler} name="firstName"/>
-          <input type="text" placeholder="Last name" onChange={this.stateHandler} name="lastName"/>
-          <input type="text" placeholder="Username" onChange={this.stateHandler}  name="username"/>
-          <input type="email" placeholder="Email" onChange={this.stateHandler} name="email"/>
-          <button type="button" onClick={this.saveUser}>Submit</button>
-        </form>
-        {this.state.users.map(user =>
-          <ul key={user._id}>
-            <li>{user.firstName} {user.lastName}</li>
-            <li>{user.username}</li>
-            <li>{user.email}</li>
-            <li onClick={this.deleteUser(user)}>Delete</li>
-          </ul>
-        )}
+        <Grid>
+          <Row>
+            <Col>
+              <h1>Users</h1>
+              <UserForm {...this.props} saveUser={this.saveUser} stateHandler={this.stateHandler} />
+              <UserList users={this.state.users} deleteUser={this.deleteUser}/>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
