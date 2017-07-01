c6792009048036466573edff2a076306f23f2ef3
Jason Stokes <jpstokes@plecco.net>
2017-07-01 11:25:33 -0400

    saving

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   state = {users: []}
 
   componentDidMount() {
-    fetch('/users')
+    fetch('/api/v1/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
